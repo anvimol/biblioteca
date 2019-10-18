49c1f8af942f2236bd4d33667b9bff595e12f0ce
anvimol <anvimol@gmail.com>
2019-10-18 17:22:04 +0200

    validando gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sistema Biblioteca
+                    Sistema Biblioteca Laravel
                 </div>
 
                 <div class="links">
